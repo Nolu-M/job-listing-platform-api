d106b822cfea82c3e0f8acb0439e8d168325f802
Nolu-M <119700411+Nolu-M@users.noreply.github.com>
2023-07-13 17:44:18 +0200

    Added 'Create email alert' route

--- Api.py
+++ Api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 import config
@@ -9,3 +9,14 @@ CORS(app)
 @app.route('/')
 def main():
     return "Girlcode Careers API"
+
+@app.route('/create-job-alert', methods=["POST"])
+def create_job_alert_route():
+    data = request.form
+    
+    job = data['job']
+    province = data['province']
+    city = data['city']
+    email = data['email']
+
+    return f'Submission from {job} {province} {city} {email}'
