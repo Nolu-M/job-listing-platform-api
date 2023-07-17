97fffef154f61d3f4332753bf89f948d53601fae
Ndamulelo Magidi <ndamulelo.magidi@icloud.com>
2023-07-17 21:46:38 +0200

    Updated Api.py and db.py for jobs form

--- Api.py
+++ Api.py
@@ -3,6 +3,9 @@ from flask_cors import CORS
 
 import config
 
+from db import db
+
+
 app = Flask(__name__)
 CORS(app)
 
@@ -10,7 +13,9 @@ CORS(app)
 def main():
     return "Girlcode Careers API"
 
-@app.route('/create-job-alert', methods=["POST"])
+
+
+@app.route('/create_job_alert', methods=["POST"])
 def create_job_alert_route():
     data = request.form
     
@@ -19,6 +24,7 @@ def create_job_alert_route():
     city = data['city']
     email = data['email']
 
+<<<<<<< HEAD
     return f'Submission from {job} {province} {city} {email}'
 
 
@@ -33,3 +39,21 @@ def submit_job_listing():
     requirements = request.form['requirements']
 
     return 'Job listing successfully submitted'
+=======
+    try:
+        email_alert_instance = db('email_alert')
+
+        email_alert_rows = email_alert_instance.select(condition=f"WHERE email='{email}'")
+        email_alert_id = None
+
+        if len(email_alert_rows):
+            email_alert_id = email_alert_rows[0][0]
+        else:
+            email_alert_id = email_alert_instance.insert("job, province, city, email", f"'{job}', '{province}', '{city}', '{email}'")
+
+    except AttributeError:
+        print('No such attribute')  
+    return f'Submission from {job} {province} {city} {email}'
+
+  
+>>>>>>> 9eaac29bb22090c19a17101b213fb187e966012d
--- sql_statements.sql
+++ sql_statements.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 CREATE TABLE job_listings (
   id INT PRIMARY KEY AUTO_INCREMENT,
@@ -8,3 +9,35 @@ CREATE TABLE job_listings (
   description VARCHAR(255) NOT NULL,
   requirements VARCHAR(255) NOT NULL,
 );
+=======
+CREATE TABLE email_alert (
+	id SERIAL PRIMARY KEY,
+	job TEXT NOT NULL,
+	province TEXT NOT NULL,
+	city TEXT NOT NULL,
+	email TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE jobs (
+	id SERIAL PRIMARY KEY,
+	job_title TEXT NOT NULL UNIQUE,
+	company TEXT NOT NULL UNIQUE,
+	job_location TEXT NOT NULL,
+	job_desc TEXT NOT NULL,
+	job_req_ TEXT NOT NULL
+);
+
+CREATE TABLE job (
+	id SERIAL PRIMARY KEY,
+	job_name TEXT NOT NULL UNIQUE,
+	job_type TEXT NOT NULL UNIQUE,
+	job_posted DATE NOT NULL
+);
+
+ALTER TABLE job
+ADD job_location TEXT;
+
+
+INSERT INTO job( job_name, job_type, job_posted, job_location)
+VALUES ('Backend engineer', 'Contract', '2023-07-14', 'Gauteng');
+>>>>>>> 9eaac29bb22090c19a17101b213fb187e966012d
